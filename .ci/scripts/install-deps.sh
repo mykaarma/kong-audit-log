4b6489926cdf40100c627bb644fd12b0f58360a9
AadiS11 <105626374+AadiS11@users.noreply.github.com>
2022-08-05 16:54:26 +0530

    Update install-deps.sh

--- .ci/scripts/install-deps.sh
+++ .ci/scripts/install-deps.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+sudo apt-get install lua5.1 liblua5.1
+
 wget https://luarocks.org/releases/luarocks-3.9.1.tar.gz
 tar zxpf luarocks-3.9.1.tar.gz
 
 cd luarocks-3.9.1
 
 echo "Running configure"
-export PATH=/usr/bin/lua:$PATH
 
 ./configure
 
